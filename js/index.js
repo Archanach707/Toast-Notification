cdb1bb4f0f4b5927badb16b1ce50b97e5aafc9bc
Archanach707 <54771334+Archanach707@users.noreply.github.com>
2022-07-03 19:27:53 +0530

    call function for toast

--- js/index.js
+++ js/index.js
@@ -9,13 +9,19 @@ const massages = [
   'Message Five',
 ]
 
-button.addEventListener('click', () => createNotification())
+button.addEventListener('click', () => createNotification('This is invalid data'))
 
 function createNotification() {
   const notif = document.createElement('div')
   notif.classList.add('toast')
 
   notif.innerText = getRandomMessage()
+
+  toasts.appendChild(notif)
+
+  setTimeout(() => {
+    notif.remove()
+  }, 3000)
 }
 
 function getRandomMessage() {
